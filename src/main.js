f8be03ce36cf5ff50f20548bbf43750ead0ce94e
mahdiawan-nk <dev.mrcoders@gmail.com>
2024-12-28 08:18:20 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import Vue3Toasity from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 
-createApp(App).use(router).mount('#app')
+router.beforeEach((to, from, next) => {
+  // Set title page sesuai meta title pada rute
+  const defaultTitle = "My Vue App"; // Judul default jika meta.title tidak ada
+  document.title = to.meta.title || defaultTitle;
+  next();
+});
+const app = createApp(App);
+app.use(
+  Vue3Toasity,
+  {
+    autoClose: 2000,
+    closeOnClick: false,
+    pauseOnHover: false,
+    pauseOnFocusLoss: false,
+    limit: 1,
+    dangerouslyHTMLString: true,
+  } // global options type definition --> ToastContainerOptions
+);
+
+app.use(router);
+app.mount("#app");
