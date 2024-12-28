991b5e0cd13396eb6c596d3914887e986c3ea511
mahdiawan-nk <dev.mrcoders@gmail.com>
2024-12-28 08:18:18 +0700

    Update Footer.vue

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer id="footer" class="footer position-relative light-background">
+    <footer id="footer" class="footer position-relative">
 
         <div class="container footer-top">
             <div class="row row-cols-1 row-cols-md-4 g-4 features">
@@ -13,29 +13,33 @@
                     <div class="features-item">
                         <i class="bi bi-instagram"
                             style="background: linear-gradient(45deg, #F58529, #DD2A7B, #8134AF, #515BD4); -webkit-background-clip: text; color: transparent;"></i>
-                        <h3><a href="" class="stretched-link">Instagaram</a></h3>
+                        <h3><a href="https://www.instagram.com/kursusbahasadankomputer_sci/profilecard/?igsh=MjNlNXR5Mnd3aHRw"
+                                class="stretched-link">Instagaram</a></h3>
                     </div>
                 </div>
                 <div class="col" data-aos="fade-up" data-aos-delay="100">
                     <div class="features-item">
                         <i class="bi bi-tiktok" style="color: #000000;"></i>
-                        <h3><a href="" class="stretched-link">Tiktok</a></h3>
+                        <h3><a href="https://www.tiktok.com/@ayokursus?_t=8sTfZBCKRyU&_r=1"
+                                class="stretched-link">Tiktok</a></h3>
                     </div>
                 </div>
                 <div class="col" data-aos="fade-up" data-aos-delay="100">
                     <div class="features-item">
-                        <i class="bi bi-facebook" style="background: linear-gradient(45deg, #1877F2, #3b5998); -webkit-background-clip: text; color: transparent;"></i>
-                        <h3><a href="" class="stretched-link">Facebook</a></h3>
+                        <i class="bi bi-facebook"
+                            style="background: linear-gradient(45deg, #1877F2, #3b5998); -webkit-background-clip: text; color: transparent;"></i>
+                        <h3><a href="https://www.facebook.com/profile.php?id=61567685371018&mibextid=ZbWKwL"
+                                class="stretched-link">Facebook</a></h3>
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="container copyright text-center mt-4">
-            <p>© <span>Copyright</span> <strong class="px-1 sitename">Ayo Kursus</strong> <span>All Rights Reserved</span>
+            <p>© <span class="text-white">Copyright</span> <strong class="px-1 sitename text-white">Ayo Kursus</strong>
+                <span class="text-white">All Rights Reserved</span>
             </p>
-            <div class="credits">
-
+            <div class="credits text-white">
                 Designed by <a href="https://bootstrapmade.com/">Mahdi-NK</a> Distributed by <a
                     href=“https://themewagon.com>ThemeWagon</a>
             </div>
@@ -46,13 +50,23 @@
     <!-- Scroll Top -->
     <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
-
+    <ToastNotif />
     <!-- Preloader -->
     <!-- <div id="preloader"></div> -->
 </template>
 
 <script>
+import ToastNotif from '@/components/ToastNotif.vue';
 export default {
-    name: 'FooterView'
+    name: 'FooterView',
+    components: {
+        ToastNotif
+    }
 }
 </script>
+
+<style scoped>
+footer {
+    background-color: #d546d9;
+}
+</style>
