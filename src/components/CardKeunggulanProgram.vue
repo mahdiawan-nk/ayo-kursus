c1d286093bf2af88c1633477847df475ecd0066e
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-01-03 04:23:41 +0700

    Update CardKeunggulanProgram.vue

--- src/components/CardKeunggulanProgram.vue
+++ src/components/CardKeunggulanProgram.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-lg-12 col-md-4 my-2" data-aos="fade-up" data-aos-delay="100">
         <div class="features-item">
-            <i :class="'bi '+icon" style="color: #ffbb2c;"></i>
+            <i :class="'bi '+icon" style="color: rgb(37, 150, 190);"></i>
             <h3><a href="#" class="stretched-link">{{ title }}</a></h3>
         </div>
     </div>
@@ -23,3 +23,7 @@
         }
     }
 </script>
+
+<style scoped>  
+
+</style>
