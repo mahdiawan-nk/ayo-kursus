6e83a2f9183281b4b4f6bb511e1d4b9c90285718
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-01-07 18:28:50 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -188,16 +188,10 @@
                     </div><!-- End Contact Form -->
                     <div class="col-lg-12">
                         <h3 data-aos="fade-up" data-aos-delay="200" class="mb-4" style="text-align: center;">Sosial Media Lembaga</h3>
-                        <div class="row row-cols-2 row-cols-md-3 g-4 features">
-                            <div class="col mt-1 d-lg-none d-md-block">
-                                <div class="features-item">
-                                    <i class="bi bi-instagram" style="font-size: 40px;background: linear-gradient(45deg, #F58529, #DD2A7B, #8134AF, #515BD4); -webkit-background-clip: text; color: transparent;"></i>
-                                    <i class="bi bi-tiktok" style="font-size: 40px;color: #000000;"></i>
-                                    <i class="bi bi-facebook" style="font-size: 40px;background: linear-gradient(45deg, #1877F2, #3b5998); -webkit-background-clip: text; color: transparent;"></i>
-                                </div>
-                            </div>
-                            <div class="col mt-1 d-none d-lg-block" @click="openNewTab('https://www.instagram.com/kursusbahasadankomputer_sci/profilecard/?igsh=MjNlNXR5Mnd3aHRw')">
-                                <div class="features-item">
+                        <div class="row row-cols-3 row-cols-md-3 g-4 features">
+                            
+                            <div class="col mt-1" @click="openNewTab('https://www.instagram.com/kursusbahasadankomputer_sci/profilecard/?igsh=MjNlNXR5Mnd3aHRw')">
+                                <div class="features-item justify-content-center">
                                     <i class="bi bi-instagram"
                                         style="font-size: 40px;background: linear-gradient(45deg, #F58529, #DD2A7B, #8134AF, #515BD4); -webkit-background-clip: text; color: transparent;"></i>
                                     <h3 class="d-none d-lg-block">
@@ -206,15 +200,15 @@
                                     </h3>
                                 </div>
                             </div>
-                            <div class="col mt-1 d-none d-lg-block" @click="openNewTab('https://www.tiktok.com/@ayokursus?_t=8sTfZBCKRyU&_r=1')">
-                                <div class="features-item">
+                            <div class="col mt-1 " @click="openNewTab('https://www.tiktok.com/@ayokursus?_t=8sTfZBCKRyU&_r=1')">
+                                <div class="features-item justify-content-center">
                                     <i class="bi bi-tiktok" style="font-size: 40px;color: #000000;"></i>
                                     <h3 class="d-none d-lg-block"><a href="javascript:void(0)"
                                             class="stretched-link">Tiktok</a></h3>
                                 </div>
                             </div>
-                            <div class="col mt-1 d-none d-lg-block" @click="openNewTab('https://www.facebook.com/profile.php?id=61567685371018&mibextid=ZbWKwL')">
-                                <div class="features-item">
+                            <div class="col mt-1 " @click="openNewTab('https://www.facebook.com/profile.php?id=61567685371018&mibextid=ZbWKwL')">
+                                <div class="features-item justify-content-center">
                                     <i class="bi bi-facebook"
                                         style="font-size: 40px;background: linear-gradient(45deg, #1877F2, #3b5998); -webkit-background-clip: text; color: transparent;"></i>
                                     <h3 class="d-none d-lg-block"><a href="javascript:void(0)"
