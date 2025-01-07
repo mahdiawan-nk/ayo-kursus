54867d5a4a4026127da2da78bcfd56b433b8f0b2
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-01-07 18:28:46 +0700

    Update Footer.vue

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -5,7 +5,7 @@
             <div class="row  features" style="background-color: rgb(37, 150, 190)">
                 <div class="col-lg-4"></div>
                 <div class="col-lg-4 mt-1" @click="openNewTab('https://wa.me/6281298600000')">
-                    <div class="features-item border-0 justify-content-center" >
+                    <div class="features-item border-0 justify-content-center bg-transparent" >
                         <i class="bi bi-whatsapp" style="color: #0dff0d;font-size: 40px;"></i>
                         <h3 class="">
                             <a href="javascript:void(0)" class="stretched-link text-white">Ruang Konsultasi</a>
