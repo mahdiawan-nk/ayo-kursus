51b8de38fde8bcb8258e47e6c246d139b789722f
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-01-10 10:59:37 +0700

    Update CardKeunggulanProgram.vue

--- src/components/CardKeunggulanProgram.vue
+++ src/components/CardKeunggulanProgram.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="col-lg-12 col-md-4 my-2" data-aos="fade-up" data-aos-delay="100">
-        <div class="features-item">
+        <div class="features-item border-0 p-0">
             <i :class="'bi '+icon" style="color: rgb(37, 150, 190);"></i>
             <h3><a href="#" class="stretched-link">{{ title }}</a></h3>
         </div>
@@ -17,6 +17,7 @@
             },
             icon:{
                 type: String,
+                default: 'bi-check-circle-fill',
                 required: false,
             },
         }
