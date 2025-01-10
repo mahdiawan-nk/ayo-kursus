9d7588b5cf21e777e2f81252b009a40f7b4f0640
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-01-10 10:59:37 +0700

    Update CardPortofolioKursus.vue

--- src/components/CardPortofolioKursus.vue
+++ src/components/CardPortofolioKursus.vue
@@ -1,36 +1,93 @@
 <template>
-    <div class="col-md-3 d-flex align-items-stretch">
-        <div class="card">
-            <div class="card-img">
-                <img :src="image" alt="..." class="img-fluid">
+    <section id="courses" class="events section" style="background-color: rgb(37, 150, 190)">
+
+        <!-- Section Title -->
+        <div class="container section-title text-center" data-aos="fade-up">
+            <p class="text-white">Portofolio Kursus</p>
+        </div><!-- End Section Title -->
+
+        <div ref="swiperContainer" :class="{
+            'container swiper init-swiper': isMobile,
+            'container': !isMobile
+        }">
+
+            <div :class="swiperWrapper">
+                <div :class="swiperSlide"
+                    v-for="({ title, subtitle, image }, index) in portofolioKursus" :key="index">
+                    <div class="card">
+                        <div class="card-img">
+                            <img :src="image" alt="..." class="img-fluid">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title"><a href="">{{ title }}</a></h5>
+                            <p class="fst-italic text-center">{{ subtitle }}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="card-body">
-                <h5 class="card-title"><a href="">{{ title }}</a></h5>
-                <p class="fst-italic text-center">{{ subtitle }}</p>
-            </div>
+
         </div>
-    </div>
+
+    </section><!-- /Courses Section -->
 </template>
 
 <script>
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+import portofolioData from '@/assets/portofolio.json';
 export default {
     name: 'CardPortofolioKursus',
-    props: {
-        title: {
-            type: String,
-            required: true
-        },
-        subtitle: {
-            type: String,
-            required: true
-        },
-        image: {
-            type: String,
-            required: true
-        },
+    data() {
+        return {
+            isMobile: false,
+            portofolioKursus: portofolioData.portofolio,
+            swiperWrapper: 'swiper-wrapper',
+            swiperSlide: 'swiper-slide d-flex align-items-stretch',
+            swiperConfig: {
+                direction: 'horizontal',
+                loop: true,
+                speed: 600,
+                autoplay: {
+                    delay: 2000,
+                },
+                slidesPerView: 'auto',
+                pagination: {
+                    el: '.swiper-pagination',
+                    type: 'bullets',
+                    clickable: true,
+                },
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 40,
+                    },
+                    1200: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                },
+            },
+        };
+    },
+    mounted() {
+        this.isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
+        if (this.isMobile) {
 
-    }
-}
+            this.initSwiper();
+        }
+        if (!this.isMobile) {
+            this.swiperWrapper = 'row';
+            this.swiperSlide = 'col-lg-3 d-flex align-items-stretch';
+        }
+        console.log('Is mobile:', this.isMobile);
+    },
+    methods: {
+        initSwiper() {
+            const swiperElement = this.$refs.swiperContainer; // Akses elemen dengan ref
+            new Swiper(swiperElement, this.swiperConfig);
+        },
+    },
+};
 </script>
 
 <style>
