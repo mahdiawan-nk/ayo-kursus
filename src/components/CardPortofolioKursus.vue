3974a5387fea8c92e3447b7a2db68e50ea8a6db4
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-01-12 16:12:21 +0700

    Update CardPortofolioKursus.vue

--- src/components/CardPortofolioKursus.vue
+++ src/components/CardPortofolioKursus.vue
@@ -1,26 +1,25 @@
 <template>
-    <section id="courses" class="events section" style="background-color: rgb(37, 150, 190)">
+    <section id="courses" class="events section pt-5 pb-1">
 
         <!-- Section Title -->
-        <div class="container section-title text-center" data-aos="fade-up">
-            <p class="text-white" style="color: rgb(37, 150, 190);text-decoration: underline;font-size:1.25rem">Portofolio Kursus</p>
+        <div class="container section-title text-center" data-aos="fade-up" style="padding-bottom:1rem">
+            <p class="" style="color: rgb(37, 150, 190);text-decoration: underline;font-size:1.25rem">Portofolio Kursus
+            </p>
         </div><!-- End Section Title -->
 
         <div ref="swiperContainer" :class="{
             'container swiper init-swiper': isMobile,
-            'container': !isMobile
-        }">
+            'container mt-3': !isMobile
+        }" style="padding:10px">
 
             <div :class="swiperWrapper">
-                <div :class="swiperSlide"
-                    v-for="({ title, subtitle, image }, index) in portofolioKursus" :key="index">
-                    <div class="card">
-                        <div class="card-img">
+                <div :class="swiperSlide" v-for="({ title, image }, index) in portofolioKursus" :key="index">
+                    <div class="card mb-0">
+                        <div class="card-img rounded" style="padding:10px;background-color: rgb(37, 150, 190);">
                             <img :src="image" alt="..." class="img-fluid">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title"><a href="">{{ title }}</a></h5>
-                            <p class="fst-italic text-center">{{ subtitle }}</p>
+                            <h5 class="card-title " style="font-size:0.9rem"><a href="">{{ title }}</a></h5>
                         </div>
                     </div>
                 </div>
@@ -48,7 +47,7 @@ export default {
                 loop: true,
                 speed: 600,
                 autoplay: {
-                    delay: 2000,
+                    delay: 200000,
                 },
                 slidesPerView: 'auto',
                 pagination: {
@@ -70,8 +69,8 @@ export default {
         };
     },
     mounted() {
-        this.isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
-        if (this.isMobile) {
+        this.isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent) || window.innerWidth <= 768;
+        if (this.isMobile || window.innerWidth <= 768) {
 
             this.initSwiper();
         }
@@ -95,9 +94,51 @@ export default {
     z-index: 10;
     background: var(--surface-color);
     border-top: 4px solid var(--surface-color);
-    padding: 5px;
+    padding: 10px 15px;
     box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.1);
-    margin-top: -60px;
+    margin-top: -100px;
     transition: 0.3s;
+    margin-bottom: 65px;
+}
+
+.events .card-img {
+    width: calc(100% + 60px);
+    margin-left: -30px;
+    overflow: hidden;
+    z-index: 9;
+    border-radius: 0;
+}
+
+.events .card-title {
+    font-weight: 700;
+    text-align: center;
+    margin-bottom: 0px;
+}
+
+@media screen and (max-width: 768px) {
+    .events .card-body {
+        z-index: 10;
+        background: var(--surface-color);
+        border-top: 4px solid var(--surface-color);
+        padding: 10px 15px;
+        box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.1);
+        margin-top: -55px;
+        transition: 0.3s;
+        margin-bottom: 25px;
+    }
+
+    .events .card-img {
+        width: calc(100% + 60px);
+        margin-left: -30px;
+        overflow: hidden;
+        z-index: 9;
+        border-radius: 0;
+    }
+
+    .events .card-title {
+        font-weight: 700;
+        text-align: center;
+        margin-bottom: 0px;
+    }
 }
 </style>
